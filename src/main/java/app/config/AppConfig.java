83e897b93fceb116838e863a9f8bfc2e58c99a46
Galactic Alex <Lehahahaigaleksus@mail.ru>
2024-03-05 20:06:00 +0300

    Patches

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,16 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+//    @Bean(name = "Dog")
+//    public static Dog getDog(){
+//        return new Dog();
+//    }
 }
